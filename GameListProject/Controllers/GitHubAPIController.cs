e18440ed9496b2267a83669368f3f54e90395b3d
Thiago Perissinotti <thiper01@gmail.com>
2022-04-27 22:46:53 -0300

    Update GitHubAPIController.cs

--- GameListProject/Controllers/GitHubAPIController.cs
+++ GameListProject/Controllers/GitHubAPIController.cs
@@ -12,9 +12,24 @@ public class GitHubAPIController : ControllerBase
 
     [Route("org")]
     [HttpGet]
-    public async Task<ActionResult<string>> githubRequest()
+    public async Task<ActionResult<string>> githubOrgReposRequest(string organization)
     {
-        string url = "https://api.github.com/orgs/Projeto-Engenharia";
+        //Desafio-Stags-2022
+        string url = "https://api.github.com/orgs/"+organization+"/repos";
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("fernandox2000", "1"));
+            return await client.GetStringAsync(url);
+        }
+    }
+
+    [Route("repos")]
+    [HttpGet]
+    public async Task<ActionResult<string>> githubRepoBranchesRequest(string organization, string repo)
+    {
+        // Desafio-Stags-2022
+        // transformation-notifier-api
+        string url = "https://api.github.com/repos/"+organization+"/"+repo+"/branches";
         using (HttpClient client = new HttpClient())
         {
             client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("fernandox2000", "1"));
